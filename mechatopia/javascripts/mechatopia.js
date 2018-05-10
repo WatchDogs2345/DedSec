4b27b3260e425f3f843cc8035bead4d8d8f58d50
Will Mavis <willard.mavis@gmail.com>
2018-05-10 17:46:38 -0500

    Add Phaser.io

---
+++ mechatopia/javascripts/mechatopia.js
@@ -0,0 +1,11 @@
+var game = new Phaser.Game('100', '100', Phaser.AUTO, 'Mechatopia', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+  game.stage.backgroundColor = "#000000";
+}
+
+function update() {
+}
