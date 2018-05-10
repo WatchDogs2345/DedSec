a897634c8b421453707da417bf9c87ce2758b5f1
Will Mavis <willard.mavis@gmail.com>
2018-05-10 18:20:39 -0500

    Use title.png for the title background

--- mechatopia/javascripts/mechatopia.js
+++ mechatopia/javascripts/mechatopia.js
@@ -1,10 +1,15 @@
 var game = new Phaser.Game('100', '100', Phaser.AUTO, 'Mechatopia', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('title', 'images/title.png');
 }
 
 function create() {
-  game.stage.backgroundColor = "#000000";
+  var sprite;
+
+  sprite = game.add.sprite(0, 0, 'title');
+  sprite.height = game.world.height;
+  sprite.width = game.world.width;
 }
 
 function update() {
